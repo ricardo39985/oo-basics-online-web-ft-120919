cad55f5272f2594ad19fba2609cf984f76865fc7
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-17 16:19:56 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,14 +1,31 @@
 class Book
-  def initialize(title, author, num, genre)
+  def initialize(title)
     @title=title
+  end
+  def title
+    @title
+  end
+  def author=(author)
     @author=author
-    @num=num
-    @genre=genre
+  end
+  def author
+    @author
+  end
+  def page_count=(num)
+    @page_count=num    
+  end
+  def page_count
+    @page_count    
+  end
+  def genre=(genre)
+    @genre=genre    
+  end
+  def genre
+    @genre    
   end
   def turn_page
-    puts "Flipping the page...wow, you read fast!"
+    puts "Flipping the page...wow, you read fast!"    
   end
 end
-ew = Book.new("And Then There Were None")
 book = Book.new("Some Title")
 book.turn_page
