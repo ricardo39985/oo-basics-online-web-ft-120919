da869d59797743d9cbe53cd1736ad18166860541
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-17 16:12:35 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,12 @@
 class Book
-  def initialize(title)
+  def initialize(title, author, num, genre)
     @title=title
-  end
-  def title
+    @author=author
+    @num=num
+    @genre=genre
   end
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
 end
+Book.new("And Then There Were None")
