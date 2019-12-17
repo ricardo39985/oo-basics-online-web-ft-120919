9d72ff7bd01eac2d50aa0a788ee652dd07659c3e
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-17 16:07:53 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
-
+class Book
+  def initialize(title, author, num, genre)
+    @title=title
+    @author=author
+    @num=num
+    @genre=genre
+    
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"    
+  end
+  
+end
 
